ecf8c4dae0a2051ee04da9751294d4e706007429
YitianChen1996 <30911432+YitianChen1996@users.noreply.github.com>
2017-09-28 19:47:48 -0400

    YitianChen

--- main.cpp
+++ main.cpp
@@ -2,37 +2,38 @@
 #include <cstring>
 #include <vector>
 #include <queue>
+#include <algorithm>
+#include <map>
 
 using namespace std;
 
 class Solution {
 private:
-    int slen1, slen2;
+    map<string, int> m, tm;
+    int wordsvlen, wlen, slen;
 
-    int lookForward(string s, int index, char c) {//see how many continuous letter c
-        int ans = 0;
-        while (s[index] == c) {
-            ans++;
-            index++;
+    void init(vector<string> &words) {
+        wordsvlen = (int) (words.size());
+        wlen = (int) (words[0].length());
+        for (int i = 0; i <= wordsvlen - 1; i++) {
+            if (m.find(words[i]) == m.end()) {
+                m.insert(pair<string, int>(words[i], 1));
+            } else {
+                m[words[i]]++;
+            }
         }
-        return ans;
     }
 
 public:
-    bool isMatch(string s, string p) {
+    vector<int> findSubstring(string s, vector<string> &words) {
+        init(words);
+        slen = (int) s.length();
+        for (int i = 0; i <= slen - wlen * wordsvlen; i++) {
+            tm = m;
+            for (int j = 0; j <= wordsvlen - 1; j++) {
+                if (tm.find(s.substr(i + j * wlen, i + (j + 1) * wlen - 1)) != tm.end()) {
 
-        slen1 = s.length();
-        slen2 = p.length();
-        int p1 = 0, p2 = 0;
-        while (1) {
-            if (s[p1] == p[p2] && p2 + 1 <= slen2 - 1 && p[p2 + 1] != '*') {
-                if (p1 == slen1 - 1 && p2 == slen2 - 1) {
-                    return true;
                 }
-                p1++;
-                p2++;
-            } else if (s[p1] == p[p2] && p2 + 1 <= slen2 - 1 && p[p2 + 1] == '*') {
-
             }
         }
     }
@@ -40,13 +41,21 @@ public:
 
 int main() {
     Solution *solution = new Solution();
-    vector<int> v;
-    int n, a;
+    map<string, int> m;
+    int n;
+    string s;
     cin >> n;
     for (int i = 0; i <= n - 1; i++) {
-        cin >> a;
-        v.push_back(a);
+        cin >> s;
+        if (m.find(s) == m.end()) {
+            m.insert(pair<string, int>(s, 1));
+        } else {
+            m[s]++;
+        }
     }
-    printf("%d", solution->jump(v));
+    while (1) {
+        cin >> s;
+        cout << m[s] << endl;
+    }
     return 0;
 }
