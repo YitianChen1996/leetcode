67f5ef526365e9e6b54ba2c86e496befbef0386e
Ubuntu <ubuntu@ip-172-26-5-137.us-east-2.compute.internal>
2018-08-22 19:52:37 +0000

    c

---
+++ code.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};           
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == NULL) {
+            return false;
+        }
+        ListNode *p1 = head, *p2 = head;
+        p1 = p1->next;
+        p2 = (p2 != NULL && p2->next != NULL) ? p2->next->next : NULL;
+        while(p1 != NULL && p2 != NULL) {
+            if (p1 == p2) {
+                return true;
+            } else {
+                p1 = p1->next;
+                p2 = (p2 != NULL && p2->next != NULL) ? p2->next->next : NULL;
+            }
+        }
+        return false;
+    }
+};
+
+int main() {
+    
+}
