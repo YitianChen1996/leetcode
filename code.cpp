55fb5989beda9624fbfb421246566775aaa60d9f
YitianChen1996 <30911432+YitianChen1996@users.noreply.github.com>
2018-08-27 09:15:14 -0400

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,83 +1,26 @@
-#include <iostream>
-#include <cstring>
-#include <vector>
-#include <queue>
-#include <deque>
-#include <algorithm>
-#include <map>
-
-using namespace std;
-
-class HitCounter {
-private:
-    deque<pair<int, int>> window; // timestamp -> number map
-    int hitCount;
-
-    void clearWindow(int lastTime) {
-        while (!window.empty() && window.front().first <= lastTime - 300) {
-            hitCount -= window.front().second;
-            window.pop_front();
-        }
-        if (window.empty()) {
-            hitCount = 0;
-        }
-    }
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
 public:
-    /** Initialize your data structure here. */
-    HitCounter() {
-        window.clear();
-        hitCount = 0;
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        
     }
-    
-    /** Record a hit.
-        @param timestamp - The current timestamp (in seconds granularity). */
-    void hit(int timestamp) {
-        if (window.empty()) {
-            window.push_back(make_pair(timestamp, 1));
-            hitCount++;
-            return;
-        }
-        if (window.back().first == timestamp) {
-            window.back().second++;           
-            hitCount++;
-        } else {
-            window.push_back(make_pair(timestamp, 1));
-            hitCount++;
-        }
-        clearWindow(window.back().first);
-    }
-    
-    /** Return the number of hits in the past 5 minutes.
-        @param timestamp - The current timestamp (in seconds granularity). */
-    int getHits(int timestamp) {
-        clearWindow(timestamp);
-        return hitCount;
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        
     }
 };
 
-/**
- * Your HitCounter object will be instantiated and called as such:
- * HitCounter obj = new HitCounter();
- * obj.hit(timestamp);
- * int param_2 = obj.getHits(timestamp);
- */
-
-int main() {
-    HitCounter *counter = new HitCounter();
-    counter->hit(1);
-    counter->hit(2);
-    counter->hit(3);
-    cout << counter->getHits(4) << endl;
-    counter->hit(300);
-    cout << counter->getHits(300) << endl;
-    cout << counter->getHits(301) << endl;
-    cout << counter->getHits(599) << endl;
-    cout << counter->getHits(600) << endl;
-    cout << counter->getHits(602) << endl;
-    return 0;
-}
-
-
-
-
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
