728cb121fc5610e7c4f54bc290d1438a2800928e
Ubuntu <ubuntu@ip-172-26-1-28.us-east-2.compute.internal>
2018-09-11 13:46:30 +0000

    c

--- code.cpp
+++ code.cpp
@@ -1,6 +1,24 @@
+#include "base.h"
+
 class Solution {
+private:
+	int accu;
+
 public:
     int numberOfArithmeticSlices(vector<int>& A) {
-        
+        if (A.size() < 3) {
+			return 0;
+		}
+		accu = 0;
+		int im1 = 0;
+		for (int i = 2; i < A.size(); i++) {
+			if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+				im1++;
+				accu += im1;
+			} else {
+				im1 = 0;
+			}
+		}
+		return accu;
     }
 };
