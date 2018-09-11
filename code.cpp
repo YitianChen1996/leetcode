a51c834d1b8311d03c828e4c8bccd60272d5639a
YitianChen1996 <30911432+YitianChen1996@users.noreply.github.com>
2018-09-11 09:34:14 -0400

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,38 +1,6 @@
-#include "base.h"
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class Solution {
-private:
-	vector<int> ans;
-
-	void rightFirstTraverse(TreeNode *node, int layer) {
-		if (node == NULL) { return; }
-		if (layer + 1 > ans.size()) {
-			ans.push_back(node->val);
-		}
-		node->rightFirstTraverse(node->right, layer + 1);
-		node->rightFirstTraverse(node->left, layer + 1);
-	}
-
 public:
-    vector<int> rightSideView(TreeNode* root) {
-		rightFirstTraverse(root, 0);
-		return ans;
+    int numberOfArithmeticSlices(vector<int>& A) {
+        
     }
 };
-
-int main() {
-    Solution *solution = new Solution();
-    vector<string> ans;
-	ans = solution->findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT");
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << endl;
-	}
-	return 0;
-}
