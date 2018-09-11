b3a7831018ec907a05fa576ca22a646981cc7c94
Ubuntu <ubuntu@ip-172-26-1-28.us-east-2.compute.internal>
2018-09-11 00:15:18 +0000

    c

--- code.cpp
+++ code.cpp
@@ -1,6 +1,24 @@
+bool cmp(int a, int b) {
+	string sa = to_string(a);
+	string sb = to_string(b);
+	int pa = 0, pb = 0;
+	while (sa[pa] == sb[pb]) {
+		pa++;
+		pb++;
+		if (pa == sa.length()) { pa = 0; }
+		if (pb == sb.length()) { pb = 0; }
+	}
+	return sa[pa] > sb[pb];
+}
+
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        
+		sort(nums.begin(), nums.end(), cmp);
+		string ans;
+		for (int i = 0; i < nums.size(); i++) {
+			ans += to_string(nums[i]);
+		}
+		return ans;
     }
 };
