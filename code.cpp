e441eb54e82b912627c5e3bf0f46a690f45fb7cc
Ubuntu <ubuntu@ip-172-26-1-28.us-east-2.compute.internal>
2018-09-11 01:48:13 +0000

    c

--- code.cpp
+++ code.cpp
@@ -1,18 +1,29 @@
 #include "base.h"
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
+private:
+	vector<int> ans;
+
+	void rightFirstTraverse(TreeNode *node, int layer) {
+		if (node == NULL) { return; }
+		if (layer + 1 > ans.size()) {
+			ans.push_back(node->val);
+		}
+		node->rightFirstTraverse(node->right, layer + 1);
+		node->rightFirstTraverse(node->left, layer + 1);
+	}
+
 public:
     vector<int> rightSideView(TreeNode* root) {
-        
+		rightFirstTraverse(root, 0);
+		return ans;
     }
 };
 
