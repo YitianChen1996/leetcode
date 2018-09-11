e3e0e381db329c80a937e0f1d73e9b2603471267
Ubuntu <ubuntu@ip-172-26-1-28.us-east-2.compute.internal>
2018-09-11 00:30:56 +0000

    c

--- code.cpp
+++ code.cpp
@@ -2,15 +2,17 @@
 
 bool cmp(int a, int b) {
     if (a == b) { return false; }
+	bool aEnd = false, bEnd = false;
     string sa = to_string(a);
     string sb = to_string(b);
     int pa = 0, pb = 0;
-    while (sa[pa] == sb[pb]) {
+    while (sa[pa] == sb[pb] && (!aEnd || !bEnd)) {
         pa++;
         pb++;
-        if (pa == sa.length()) { pa = 0; }
-        if (pb == sb.length()) { pb = 0; }
+        if (pa == sa.length()) { pa = 0; aEnd = true; }
+        if (pb == sb.length()) { pb = 0; bEnd = true; }
     }
+	if (aEnd && bEnd) { return false; }
     return sa[pa] > sb[pb];
 }
 
